d5f698a2c38d2a4ee8f915833fb3cb12c8de5216
CryFish <glebbogomolov04@gmail.com>
2023-03-26 22:52:18 +0300

    Добавьте файлы проекта.

---
+++ WPF_TheKeeper/Forms/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WPF_TheKeeper.Entities;
+
+namespace WPF_TheKeeper.Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Request _currentRequest = new Request();
+
+        public MainWindow(Employee employee)
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new PageList(employee.AccessCode));
+            Manager.MainFrame = MainFrame;
+            using (var db = new TheKeeperPRODataBaseEntities())
+            {
+                Role role = db.Roles.AsNoTracking().FirstOrDefault(u => u.id == employee.id);
+            }
+            DataContext = _currentRequest;
+        }
+
+        
+    }
+}
---
+++ WPF_TheKeeper/Forms/PageList.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_TheKeeper.Entities;
+
+namespace WPF_TheKeeper.Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для PageList.xaml
+    /// </summary>
+    public partial class PageList : Page
+    {
+        public PageList(int role)
+        {
+            InitializeComponent();
+            if (role == 2)
+            {
+                List<Request> requests = TheKeeperPRODataBaseEntities.GetContext().Requests.ToList();
+                for (int i=0; i <requests.Count; i++)
+                {
+                    if (requests[i].StatusRequestCode != 2) requests.RemoveAt(i);
+                }
+                DGRequest.ItemsSource = requests;
+            }
+        }
+        private void ButView_Click(object sender, RoutedEventArgs e)
+        {
+            Request request = (sender as Button).DataContext as Request;
+            int code = request.id;
+            using (var db = new TheKeeperPRODataBaseEntities())
+            {
+                Request requests = db.Requests.AsNoTracking().FirstOrDefault(u => u.id == code);
+                if (requests.BlackList == false)
+                {
+                    MessageBox.Show("Заявка отсутсвует в чёрном списке!");
+                    if (requests.TypeRequestCode == 1)
+                    {
+                        SoloRequestWindow soloRequestWindow = new SoloRequestWindow(requests, code);
+                        soloRequestWindow.Show();
+                    }
+                    else
+                    {
+                        GroupRequestWindow groupRequestWindow = new GroupRequestWindow(requests, code);
+                        groupRequestWindow.Show();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Заявка находится в чёрном списке!");
+                    return;
+                }
+            }
+        }
+
+        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                TheKeeperPRODataBaseEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGRequest.ItemsSource = TheKeeperPRODataBaseEntities.GetContext().Requests.ToList();
+            }
+        }
+    }
+}
